4169ed96a5473a0d8c02b3d90ba93bd0761fb971
amstu2 <amstu2@student.monash.edu>
2018-09-22 14:48:33 +1000

    Added state machine

--- Test.X/main.c
+++ Test.X/main.c
@@ -41,23 +41,84 @@
     MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
     TERMS.
 */
+
 #define FOSC    (7370000ULL)
 #define FCY     (FOSC/2)
 #include <libpic30.h>
 #include "mcc_generated_files/mcc.h"
 
+#define QUARTER_DUTY_STATE  0
+#define HALF_DUTY_STATE     1
+#define FULL_DUTY_STATE     2
+
+#define FIFTY_HERTZ_STATE   0
+#define ONE_K_HERTZ_STATE   1
+#define TEN_K_HERTZ_STATE   2
+
+
 /*
                          Main application
  */
 
+void DutyStateMachine(unsigned char state)
+{
+    IO_RB12_SetHigh();
+    IO_RB13_SetHigh();
+    IO_RB14_SetHigh();
+    IO_RB15_SetHigh();
+    switch(state)
+    {
+        case QUARTER_DUTY_STATE:
+            IO_RB15_SetLow();
+            break;
+        case HALF_DUTY_STATE:
+            IO_RB14_SetLow();
+            break;
+        case FULL_DUTY_STATE:
+            IO_RB13_SetLow();
+            break;
+    }
+}
+
+void FreqStateMachine(unsigned char state)
+{
+    IO_RB12_SetHigh();
+    IO_RB13_SetHigh();
+    IO_RB14_SetHigh();
+    IO_RB15_SetHigh();
+    switch(state)
+    {
+        case FIFTY_HERTZ_STATE:
+            IO_RB15_SetLow();
+            break;
+        case ONE_K_HERTZ_STATE:
+            IO_RB14_SetLow();
+            break;
+        case TEN_K_HERTZ_STATE:
+            IO_RB13_SetLow();
+            break;
+    }
+}
+
+int ButtonPressed(uint16_t value)
+{
+    if(value == 0) return true;
+    else return false;
+}
+
 int main(void)
 {
-    uint16_t test_duty = 0x0000;
-    uint8_t test_byte = 0xCC; 
-    uint8_t received_byte = 0x00;
+    unsigned char duty_cycle_state = 0;
+    unsigned char freq_cycle_state = 0;
+    unsigned char duty_button_enabled = 1;
+    unsigned char freq_button_enabled = 1;
     // initialize the device
     SYSTEM_Initialize();
+    uint16_t duty_input_value;
+    uint16_t freq_input_value;
 
+
+    // Read RB5
     // When using interrupts, you need to set the Global Interrupt Enable bits
     // Use the following macros to:
 
@@ -66,25 +127,48 @@ int main(void)
 
     // Disable the Global Interrupts
     //INTERRUPT_GlobalDisable();
-    IO_RB12_SetLow();
-    IO_RB13_SetLow();
-    //IO_RB14_SetLow();
-    //IO_RB15_SetLow();
-    __delay_ms(3000);
+    IO_RB12_SetHigh();
+    IO_RB13_SetHigh();
+    IO_RB14_SetHigh();
+    IO_RB15_SetHigh();
+    
     while (1)
     {
-        received_byte = SPI1_Exchange8bit(test_byte);
-        if(received_byte == 0xBB)
-        {
-            IO_RB12_Toggle();
-            IO_RB13_Toggle();
-            received_byte = 0xAA;
-        }
-        //PWM_DutyCycleSet(1,test_duty);
-        //test_duty += 0x3FFF;
-        //if(test_duty > 0xFFFF) test_duty = 0x0000;
-        __delay_ms(1000);
+        duty_input_value = DUTY_INPUT_GetValue();
+        freq_input_value = FREQ_INPUT_GetValue();
         
+        if(ButtonPressed(DUTY_INPUT_GetValue()))
+        {
+            IO_RB12_SetHigh();
+            if(duty_button_enabled)
+            {
+                duty_cycle_state++;
+                if(duty_cycle_state == 3) duty_cycle_state = 0;
+                DutyStateMachine(duty_cycle_state);
+                duty_button_enabled = 0;
+            }
+        }
+        else
+        {
+            duty_button_enabled = 1;
+            IO_RB12_SetLow();
+        }
+        if(ButtonPressed(FREQ_INPUT_GetValue()))
+        {
+            if(freq_button_enabled)
+            {
+                freq_cycle_state++;
+                if(freq_cycle_state == 3) freq_cycle_state = 0;
+                FreqStateMachine(freq_cycle_state);
+                freq_button_enabled = 0;
+            }
+        }
+        else
+        {
+            freq_button_enabled = 1;
+            IO_RB12_SetLow();
+        }
+        __delay_ms(50);
     }
 }
 /**
